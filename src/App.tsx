35667e3c1e3283400da04b16fea3fa5d991cc1d1
PreetamGup <guptapritam285@gmail.com>
2023-09-12 18:23:04 +0530

    latest

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App ">
-      <h1 className='text-center bg-slate-950 text-white'>React</h1>
+      <h1 className='text-center bg-slate-950 text-white'>Assignmet</h1>
     </div>
   );
 }
